3d2cb24041370f2ef0ab9bec6e4771d532e57232
mreshtaiwi <mr.eshtaiwi@gmail.com>
2023-05-10 20:51:18 +0300

    react app

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Memes App</Navbar.Brand>
+        <Nav className="me-auto">
+          {/* <Link to="/">Home</Link> no refresh */}
+          {/* <Link to="/favlist">Fav List</Link> */}
+          <Nav.Link href="/">Home</Nav.Link>{/*will do a full page refresh*/}
+          <Nav.Link href="/favlist">Fav List</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header;
