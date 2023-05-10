c09da4e6f47c277345ae603842fb8775d37dec67
mreshtaiwi <mr.eshtaiwi@gmail.com>
2023-05-10 21:30:39 +0300

    fixing route

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,17 @@ import Container from 'react-bootstrap/Container';
 
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">Memes App</Navbar.Brand>
         <Nav className="me-auto">
-          {/* <Link to="/">Home</Link> no refresh */}
-          {/* <Link to="/favlist">Fav List</Link> */}
-          <Nav.Link href="/">Home</Nav.Link>{/*will do a full page refresh*/}
-          <Nav.Link href="/favlist">Fav List</Nav.Link>
+          <Link to="/">Home</Link> no refresh
+          <Link to="/favMeme">Fav List</Link>
+          {/* <Nav.Link href="/">Home</Nav.Link>will do a full page refresh */}
+          {/* <Nav.Link href="/favlist">Fav List</Nav.Link> */}
         </Nav>
       </Container>
     </Navbar>
